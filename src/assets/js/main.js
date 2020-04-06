bb3cf7dd7615aabec59ac43e00da7a039ba79577
Lili-lepage <Lili-Lepage@users.noreply.github.com>
2020-04-06 11:53:58 +0200

    first
    Home Page

---
+++ src/assets/js/main.js
@@ -0,0 +1,11 @@
+$(document).ready(function() {
+
+
+
+  $('#toggle').click(function() {
+     $(this).toggleClass('active');
+     $('#overlay').toggleClass('open');
+     $('.main-nav').toggleClass('open-nav');
+    });
+
+});

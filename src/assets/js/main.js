b43b8a7bd63958cf39af96a4ad885b0436d74857
Tanguy Le Roux <tanguy@viaduc.fr>
2020-04-06 17:41:23 +0200

    Js incremental functionality

--- src/assets/js/main.js
+++ src/assets/js/main.js
@@ -9,12 +9,27 @@ $(document).ready(function() {
     });
 
 
-/*** Compteur **/
+    /*** Compteur **/
+    $('.incremental').each(function() {
+        var object        = $(this),
+            offsetTop     = object.offset().top,
+            windowPercent = ($(window).height() / 100) * 70,
+            max           = object.attr('rel'),
+            triggered     = false;
 
-$(window).scroll(function(){
-    if( $(window).scrollTop() == 1000){
-      alert('ok');
-    }
-  });
+        $(window).scroll(function() {
+            if ($(window).scrollTop() + windowPercent > offsetTop && ! triggered) {
+                triggered = true;
 
+                var incrementalInterval = setInterval(function() {
+                    if (parseInt(object.html()) < max) {
+                        var nb = parseInt(object.html()) + 1;
+                        object.html(parseInt(object.html()) + 1);
+                    } else {
+                        clearInterval(incrementalInterval);
+                    }
+                }, 100);
+            }
+        });
+    });
 });

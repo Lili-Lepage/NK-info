fa080a0fe31f8d00a17f958be3002e3646db72fa
Lili-lepage <Lili-Lepage@users.noreply.github.com>
2020-04-06 17:03:12 +0200

    home-page V2

--- src/assets/js/main.js
+++ src/assets/js/main.js
@@ -1,11 +1,20 @@
 $(document).ready(function() {
 
-
+/***Navigation ***/
 
   $('#toggle').click(function() {
      $(this).toggleClass('active');
      $('#overlay').toggleClass('open');
      $('.main-nav').toggleClass('open-nav');
     });
+
+
+/*** Compteur **/
+
+$(window).scroll(function(){
+    if( $(window).scrollTop() == 1000){
+      alert('ok');
+    }
+  });
 
 });

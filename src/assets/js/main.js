a2797800889c0dba59eaf38ccc80de9f8bc2ad08
Lili-lepage <Lili-Lepage@users.noreply.github.com>
2020-05-01 13:52:24 +0200

    01/05
    p

--- src/assets/js/main.js
+++ src/assets/js/main.js
@@ -70,102 +70,34 @@ $('#toggle').click(function() {
     });
 
 
- /***** slider ****/
- function defer(method) {
-if (window.jQuery)
-  method();
-else
-  setTimeout(function() {
-    defer(method)
-  }, 50);
-}
-defer(function() {
-(function($) {
-
-  function doneResizing() {
-    var totalScroll = $('.slider-frame').scrollLeft();
-    var itemWidth = $('.slider-item').width();
-    var difference = totalScroll % itemWidth;
-    if ( difference !== 0 ) {
-      $('.slider-frame').animate({
-        scrollLeft: '-=' + difference
-      }, 500, function() {
-        checkArrows();
-      });
-    }
-  }
-
-  function checkArrows() {
-    var totalWidth = $('.slider .slider-item').length * $('.slider-item').width();
-    var frameWidth = $('.slider-frame').width();
-    var itemWidth = $('.slider-item').width();
-    var totalScroll = $('.slider-frame').scrollLeft();
-
-   // if ( ((totalWidth - frameWidth) - totalScroll) < itemWidth ) {
-   //   $(".next").css("visibility", "hidden");
-   // }
-   // else {
-   //   $(".next").css("visibility", "visible");
-   // }
-   // if ( totalScroll < itemWidth ) {
-   //   $(".prev").css("visibility", "hidden");
-   // }
-   // else {
-   //   $(".prev").css("visibility", "visible");
-   // }
-  }
-
-  $('.arrow').on('click', function() {
-    var $this = $(this),
-      width = $('.slider-item').width(),
-      speed = 500;
-    if ($this.hasClass('prev')) {
-      $('.slider-frame').animate({
-        scrollLeft: '-=' + width
-      }, speed, function() {
-        checkArrows();
-      });
-    } else if ($this.hasClass('next')) {
-      $('.slider-frame').animate({
-        scrollLeft: '+=' + width
-      }, speed, function() {
-        checkArrows();
-      });
-    }
-  });
-
-  $(window).on("load resize", function() {
-    checkArrows();
-    $('.slider .slider-item').each(function(i) {
-      var $this = $(this),
-        left = $this.width() * i;
-      $this.css({
-        left: left
-      })
-    });
-  });
-
-  var resizeId;
-  $(window).resize(function() {
-      clearTimeout(resizeId);
-      resizeId = setTimeout(doneResizing, 500);
-  });
-
-})(jQuery);
-});
-
-
-/**** Temoignages *****/
+/**** Slider *****/
 
 $("#testimonial-slider").owlCarousel({
         items:3,
         itemsDesktop:[1000,2],
         itemsDesktopSmall:[980,1],
         itemsTablet:[768,1],
-        pagination:true,
+        pagination:false,
         navigation:true,
-        navigationText:["<",">"],
-        autoPlay:true
+        navigationText:["",""],
+        rewindNav : true,
+        scrollPerPage : false,
+        autoPlay:true,
+        responsive: true
     });
+
+    $("#logo-slider").owlCarousel({
+            items:5,
+            itemsDesktop:[1000,2],
+            itemsDesktopSmall:[980,1],
+            itemsTablet:[768,1],
+            pagination:false,
+            navigation:true,
+            navigationText:["",""],
+            rewindNav : true,
+            scrollPerPage : false,
+            autoPlay:true,
+            responsive: true
+        });
 
 });

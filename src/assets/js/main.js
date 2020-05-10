7cf9b915a7503cf56d161fd0226bb9ff69909ba1
Lili-lepage <Lili-Lepage@users.noreply.github.com>
2020-05-10 17:47:56 +0200

    References

--- src/assets/js/main.js
+++ src/assets/js/main.js
@@ -33,7 +33,8 @@ $('#toggle').click(function() {
 
     /*** Scroll vers la section 1 ***/
 
-    $('#scroll').click(function(){
+    $('#scroll').click(function(event){
+       event.preventDefault();
            var target = $("#section-1");
            if(target.length ) {
                $("html, body").stop().animate( { scrollTop: target.offset().top }, 800);
@@ -99,5 +100,31 @@ $("#testimonial-slider").owlCarousel({
             autoPlay:true,
             responsive: true
         });
+
+
+/************* FILTER *******/
+var Shuffle = window.Shuffle;
+var element = document.querySelector('.my-shuffle-container');
+var sizer = element.querySelector('.my-sizer-element');
+
+var shuffleInstance = new Shuffle(element, {
+  itemSelector: '.picture-item',
+  sizer: sizer // could also be a selector: '.my-sizer-element'
+});
+// shuffleInstance.filter('animal');
+$("#all").on("click", function(){
+   shuffleInstance.filter();
+});
+$("#btn-animal").on("click", function(){
+   shuffleInstance.filter('animal');
+});
+$("#btn-city").on("click", function(){
+   shuffleInstance.filter('city');
+});
+$("#btn-nature").on("click", function(){
+   shuffleInstance.filter('nature');
+});
+
+
 
 });
